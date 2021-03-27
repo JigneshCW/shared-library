c98ca9d8ac6f35056dead6b4bdb209e3113bbe6d
jigneshmirani <contact2jignesh@gmail.com>
2021-03-27 11:43:33 +0530

    Update helloword.groovy

--- vars/helloword.groovy
+++ vars/helloword.groovy
@@ -1,7 +1,7 @@
 def call(Map config=[:], Closure body) {
     pipeline {
         //checkout scm
-        stages {
+       
         stage('Install') {
             echo 'Tools has been installed'
         }
@@ -15,7 +15,7 @@ def call(Map config=[:], Closure body) {
             }
            
         }
-        }
+       
         //body()
     }
 }
