bc171b715653bb5a140b4fb6d6a4ca662a09bf1a
jigneshmirani <contact2jignesh@gmail.com>
2021-03-27 13:59:51 +0530

    Update jenkinsfile.groovy

--- vars/jenkinsfile.groovy
+++ vars/jenkinsfile.groovy
@@ -34,7 +34,7 @@ def call() {
                 {
 			//echo Notify.formatThis('test')
 			echo "Job Name 1 : ${env.JOB_NAME}"
-			echo  "Job Name : " +  Notify.mailBody('${evn.JOB_NAME}"'
+			echo  "Job Name : " +  Notify.mailBody('${evn.JOB_NAME}')
                     //echo mailBody()
                    // mail bcc: '', body: '' + notify1.mailBody() , cc: '', charset: 'UTF-8', from: '', mimeType: 'text/html', replyTo: '', subject: "Build Status ${currentBuild.currentResult}: Project name -> ${env.JOB_NAME}", to: "jignesh.mirani@onlinepsbloans.com"
                 }
