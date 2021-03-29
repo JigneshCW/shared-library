3776b7caa0beb5a9d77a2e26e42acb37f2010a26
jigneshmirani <contact2jignesh@gmail.com>
2021-03-29 12:56:00 +0530

    Update jenkinsfile.groovy

--- vars/jenkinsfile.groovy
+++ vars/jenkinsfile.groovy
@@ -17,7 +17,7 @@ def call() {
                 git 'https://github.com/jigneshmirani/Service-User.git'
 
                 // Run Maven on a Unix agent.
-                sh "mvn -Dmaven.test.failure.ignore=true clean package"
+                sh "mvn clean package"
 
                 // To run Maven on a Windows agent, use
                 // bat "mvn -Dmaven.test.failure.ignore=true clean package"
@@ -26,10 +26,7 @@ def call() {
             post {
                 // If Maven was able to run the tests, even if some of the test
                 // failed, record the test results and archive the jar file.
-                success {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts artifacts:'target/*.jar',fingerprint: true
-                }
+               
                 always
                 {
 			//echo Notify.formatThis('test')
