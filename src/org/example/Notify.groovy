0bfd5e4b04e2a597c2bc524773186765b5884f87
jigneshmirani <contact2jignesh@gmail.com>
2021-03-29 12:53:35 +0530

    Update Notify.groovy

--- src/org/example/Notify.groovy
+++ src/org/example/Notify.groovy
@@ -1,11 +1,11 @@
 package org.example
 
 class Notify{
-  def static String mailBody(String currntBuild){
+  def static String mailBody(currntBuild){
         
-    println currntBuild
+    //println currntBuild
     // return CurrentBuild[0].toString()
-   println currntBuild 
+   println currntBuild.toString()
   }
 
 
