13dcfe0cfeda280e450456b051e88e6610bbb71d
jigneshmirani <contact2jignesh@gmail.com>
2021-03-29 16:57:22 +0530

    Update SendMail.groovy

--- vars/SendMail.groovy
+++ vars/SendMail.groovy
@@ -1,36 +1,13 @@
-def call(String recipients) {
- def status, logRegex
-
-    switch (currentBuild.currentResult) {
-        case 'SUCCESS':
-            status = 'successed'
-            logRegex = 'SUCCESS'
-            break
-
-        case 'UNSTABLE':
-            status = 'unstable'
-            logRegex = 'FAILURE'
-            break
-
-        case 'FAILURE':
-            status = 'failed'
-            logRegex = 'FAILURE'
-            break
-
-        case 'ABORTED':
-            status = 'canceled'
-            logRegex = 'ABORTED'
-            break
-
-    }
+def call() {
+ 
   def mailBody = "<html><table border='1' width='100%'>"
     mailBody += "<tr><td colspan='2'><b>Build Status :</b></td></tr>"
     mailBody += "<tr><td width='25%'><b>Project : </b></td><td width='75%'> ${env.JOB_NAME} </td></tr>"
     mailBody += "<tr><td width='25%'><b>Build Number : </b></td><td width='75%'> ${env.BUILD_NUMBER} </td></tr>"
-    mailBody += "<tr><td width='25%'><b>URL build : </b></td><td width='75%'>Display Name : ${currentBuild.displayName} </td></tr>"
+    mailBody += "<tr><td width='25%'><b>URL build : </b></td><td width='75%'> ${env.BUILD_URL} </td></tr>"
     mailBody += "<tr><td colspan='2'><b>Change Logs :</b></td></tr>"
 
-    MAX_MSG_LEN = 100
+    def MAX_MSG_LEN = 100
     //def changeString = "<b>Changes :</b> <br/>"
     def haveChages = "No"
 
