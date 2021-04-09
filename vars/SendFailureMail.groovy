a767ceeb7e6c1963d118c149adb26b9671caaa97
jigneshmirani <contact2jignesh@gmail.com>
2021-04-09 18:56:21 +0530

    Create SendFailureMail.groovy

---
+++ vars/SendFailureMail.groovy
@@ -0,0 +1,11 @@
+def call(String msg) {
+ 
+  def mailBody = "<html><table border='1' width='100%'>"
+    mailBody += "<tr><td colspan='2'><b>Build Status :</b></td></tr>"
+    mailBody += "<tr><td width='25%'><b>Project : </b></td><td width='75%'> ${env.JOB_NAME} </td></tr>"
+    mailBody += "<tr><td width='25%'><b>Build Number : </b></td><td width='75%'> ${env.BUILD_NUMBER} </td></tr>"
+    mailBody += "<tr><td width='25%'><b>URL build : </b></td><td width='75%'> ${env.BUILD_URL} </td></tr>"
+    mailBody += "<tr><td colspan='25%'><b>Failure Reason :</b></td><td width='75%'> " + msg + "</td></tr>"
+    mailBody += "</table></html>"
+    return mailBody
+}
